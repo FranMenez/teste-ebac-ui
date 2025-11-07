d8819ac76a890abb6ad1f2041506d5d6da63a130
Francine Menez <franmenez@yahoo.com.br>
2025-11-07 14:29:56 -0300

    mudança no gitignore

--- cypress/e2e/produtos.cy.js
+++ cypress/e2e/produtos.cy.js
@@ -1,20 +1,52 @@
 /// <reference types="cypress"/>
+import produtosPage from "../../support/produtos.page";
 
 describe('funcionalidade: Produtos', () => {
 
     beforeEach(() => {
-        cy.visit('produtos')
-    });
+        produtosPage.visitarUrl()
+     });
     it('Deve selecionar um produto da lista', () => {
-        cy.get('.products > .row')
-       //.first()
-       //.last()
-       //.eq(2)
-       .contains('Aether Gym Pant')
-       .click()
-
+        produtosPage.buscarprodutoLista('Beaumont Summit Kit')
        cy.get('#tab-title-description > a').should('contain','Descrição')
         
     });
+
+    it('Deve buscar um produto com sucesso', () => {
+        let produto = 'Zeppelin Yoga Pant'
+        produtosPage.buscarproduto(produto)
+        cy.get('.product_title').should('contain',produto)
+
+        });
+        
+
+    it('Deve visitar a página do produto', () => {
+        produtosPage.visitarProduto('Zeppelin-Yoga-Pant')
+        cy.get('.product_title').should('contain', 'Zeppelin Yoga Pant')
+    });
+
+    it('Deve adicionar produto ao carrinho', () => {
+        let qtd = 7
+        produtosPage.buscarproduto('Abominable Hoodie')
+        produtosPage.addProdutoCarrinho('M','Blue', qtd)
+        
+        cy.get('woocommerce-message').should('contain', qtd + ' x  " Abominable Hoodie" foram adicionados ao carrinho')
+
+    });
     
+    it.only('Deve adicionar produto ao carrinho buscando da massa de dados', () => {
+        cy.fixture('produtos').then(dados => {
+        produtosPage.buscarproduto(dados[0].nomeProduto)
+        produtosPage.addProdutoCarrinho(
+            dados[0].tamanho,
+            dados[0].cor, 
+            dados[0].quantidade)
+        
+        cy.get('woocommerce-message').should('contain', dados[0].nomeProduto )
+
+        
+    });
+
+   });
+
 });
